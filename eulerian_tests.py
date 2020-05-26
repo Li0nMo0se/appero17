9ecf8f4ccedd95d388966bc9398e60e64caa8bb3
Epita <kenz.narainen@epita.fr>
2020-05-26 14:01:58 +0200

    all tests

--- eulerian_tests.py
+++ eulerian_tests.py
@@ -7,6 +7,14 @@ class MyTestCase(unittest.TestCase):
         edges = [(0, 1, 0), (0, 2, 0), (0, 3, 0), (0, 4, 0), (1, 2, 0), (3, 1, 0), (4, 1, 0), (3, 2, 0), (2, 4, 0),
                  (4, 3, 0)]
         self.assertEqual(is_eulerian_non_oriented(5, edges), True)
+    def test_even_vertices(self):
+        edges = [(0,2),(2,3),(3,0),(2,0),(0,1),(0,0)]
+
+        self.assertEqual(even_vertices(4,edges),False);
+    def test_find_eulerian_cycle_not_oriented(self):
+        edges = [(0,1),(1,2),(2,3),(3,0)]
+
+        self.assertEqual(find_eulerian_cycle_non_oriented(4,edges),True);
 
 
 if __name__ == '__main__':
