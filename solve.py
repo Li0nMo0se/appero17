68d44b8c12446dee17e6cb466c82d05ffcd7aaef
Ilan Guenet <ilan.guenet@epita.fr>
2020-05-26 13:12:26 +0200

    [SOLVE][UPDATE] implement the is_eulerian_non_oriented

--- solve.py
+++ solve.py
@@ -13,16 +13,58 @@
 '''
 
 
-def is_eulerian_non_oriented(num_vertices, edge_list):
+def adjacency_list(num_vertices, edges_list, is_oriented=False):
+    succ = [[] for a in range(num_vertices)]
+    for (a, b) in edges_list:
+        succ[a].append(b)
+        if not is_oriented:
+            succ[b].append(a)
+    return succ
+
+def is_eulerian_non_oriented(num_vertices, edges_list):
+    '''
+        A graph is eulerian if it has a eulerian cycle.
+        Or if all degrees are even and the edges are all connected
+    :param num_vertices:
+    :param edges_list:
+    :return: boolean whether the graph is eulerian
+    '''
+    def even_vertices(num_vertices, edges_list):
+        deg = [0] * num_vertices
+        for (a, b) in edges_list:
+            deg[a] += 1
+            deg[b] += 1
+        for a in range(num_vertices):
+            if deg[a] % 2 != 0:  # odd deg, incorrect
+                return False
+        return True
+
+    def is_edge_connected(num_vertices, edges_list):
+        if num_vertices == 0 or len(edges_list) == 0:
+            return True
+        succ = adjacency_list(num_vertices, edges_list)
+        seen = [False] * num_vertices
+        init = edges_list[0][0] # random vertex
+        seen[init] = True
+        todo = [init]
+        while todo:
+            s = todo.pop()
+            for d in succ(s):
+                if not seen[d]:
+                    seen[d] = True
+                    todo.append(d)
+        return all(seen[a] or not succ[a] for a in range(num_vertices))
+
+    return even_vertices(num_vertices, edges_list) and is_edge_connected(num_vertices, edges_list)
+
+def find_eulerian_cycle_non_oriented(num_vertices, edges_list):
     return False
 
-def find_eulerian_cycle_non_oriented(num_vertices, edge_list):
-    return False
 
-def solve(is_oriented, num_vertices, edge_list):
+def solve(is_oriented, num_vertices, edges_list):
     if not is_oriented:
-        if is_eulerian_non_oriented(num_vertices, edge_list):
-            return find_eulerian_cycle_non_oriented(num_vertices, edge_list)
+        if is_eulerian_non_oriented(num_vertices, edges_list):
+            return find_eulerian_cycle_non_oriented(num_vertices, edges_list)
         else:
             raise NotImplementedError
     else:
