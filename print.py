9b0eb52f8b70326a585ba6817254f4378a132744
antoineclaudel <antoine.claudel@hotmail.fr>
2020-06-02 15:57:09 +0200

    [ADD] print graph

---
+++ print.py
@@ -0,0 +1,20 @@
+from graphviz import Digraph
+from graphviz import Graph
+
+# open a pdf with the graph printed
+def print_graph(num_vertices, edges_list, is_oriented = False):
+    
+    if (is_oriented):
+        dot = Digraph('graph')
+    else:
+        dot = Graph('graph')
+
+    for i in range(0, num_vertices):
+        dot.node(str(i), str(i))
+
+    for (a,b) in edges_list:
+        dot.edge(str(a),str(b))
+
+    dot.view()
+
+print_graph(2, [(0,1)])
