fcce0a144469cf46118de05be99c234b4329af14
Ilan Guenet <ilan.guenet@epita.fr>
2020-06-09 10:28:59 +0200

    [UPDATE][PRINT] Print the edge's label

--- print.py
+++ print.py
@@ -1,18 +1,20 @@
 from graphviz import Digraph
 from graphviz import Graph
 
-# open a pdf with the graph printed
-def print_graph(num_vertices, edges_list, is_oriented = False):
-    
-    if (is_oriented):
-        dot = Digraph('graph')
+
+def print_graph(num_vertices, edges_list, is_oriented=False, name="graph"):
+    """
+        open a pdf with the graph printed
+    """
+    if is_oriented:
+        dot = Digraph(name)
     else:
-        dot = Graph('graph')
+        dot = Graph(name)
 
     for i in range(0, num_vertices):
         dot.node(str(i), str(i))
 
-    for (a,b) in edges_list:
-        dot.edge(str(a),str(b))
+    for (a, b, label) in edges_list:
+        dot.edge(str(a), str(b), str(label))
 
     dot.view()
