8908d673239324bff034e7d64ed8c188694cea66
Ilan Guenet <ilan.guenet@epita.fr>
2020-06-11 14:15:29 +0200

    [ADD][GRAPH] Add graph.py (print, save and convert graphs)

---
+++ graph.py
@@ -0,0 +1,71 @@
+from graphviz import Digraph
+from graphviz import Graph
+import osmnx as ox
+
+
+def make_graph(num_vertices, edges_list, is_oriented=False, name="graph"):
+    """
+    Convert a graph to the dot format
+    :param num_vertices:
+    :param edges_list:
+    :param is_oriented:
+    :param name:
+    :return: the graph represented by the dot format
+    """
+    if is_oriented:
+        dot = Digraph(name)
+    else:
+        dot = Graph(name)
+
+    for i in range(0, num_vertices):
+        dot.node(str(i), str(i))
+
+    for (a, b, label) in edges_list:
+        dot.edge(str(a), str(b), str(label))
+    return dot
+
+
+def print_graph(num_vertices, edges_list, is_oriented=False, name="graph"):
+    """
+    open a pdf with the graph printed
+    :param num_vertices:
+    :param edges_list:
+    :param is_oriented:
+    :param name:
+    """
+    dot = make_graph(num_vertices, edges_list, is_oriented, name)
+    dot.view()
+
+
+def print_graph(num_vertices, filename, edges_list, is_oriented=False,
+                name="graph"):
+    """
+    Save a graph in a file
+    :param num_vertices:
+    :param filename:
+    :param edges_list:
+    :param is_oriented:
+    :param name:
+    """
+    dot = make_graph(num_vertices, edges_list, is_oriented, name)
+    dot.save(filename + ".gv")
+
+
+def to_reg_graph(osmnx_graph, is_oriented=False):
+    """
+    Convert an osmnx graph to a regular graph (the ones used in the
+    snowymontreal library
+    :param osmnx_graph:
+    :param is_oriented:
+    :return:
+    """
+    if not is_oriented:
+        osmnx_graph = ox.utils_graph.get_undirected(osmnx_graph)
+    nodes = list(osmnx_graph.nodes)
+    osmnx_edges = list(osmnx_graph.edges)
+    edges = []
+    for elt in osmnx_edges:
+        l = ox.utils_graph.get_route_edge_attributes(osmnx_graph,
+            (elt[0], elt[1]), 'length')
+        edges.append((nodes.index(elt[0]), nodes.index(elt[1]), l[0]))
+    return len(nodes), edges
