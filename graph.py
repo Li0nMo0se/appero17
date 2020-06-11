c805ea91afdc7f83a3f415d044d2fb9a7156b552
Ilan Guenet <ilan.guenet@epita.fr>
2020-06-11 16:23:01 +0200

    [UPDATE][GRAPH] Generate a random fully connected graph with a specific number of vertices

--- graph.py
+++ graph.py
@@ -1,6 +1,22 @@
 from graphviz import Digraph
 from graphviz import Graph
+import networkx as nx
+import random
 import osmnx as ox
+
+
+def generate_connected_undirected_graph(num_vertices, directed=False):
+    """
+    Generate a random graph with num_vertices and num_edges
+    :param num_vertices:
+    :return: a graph
+    """
+
+    G = nx.erdos_renyi_graph(num_vertices, 0.5, seed=123, directed=directed)
+    edges_list = []
+    for (u, v) in G.edges():
+        edges_list.append((u, v, random.randint(0, 1000)))
+    return len(G.nodes), edges_list
 
 
 def make_graph(num_vertices, edges_list, is_oriented=False, name="graph"):
@@ -69,3 +85,4 @@ def to_reg_graph(osmnx_graph, is_oriented=False):
             (elt[0], elt[1]), 'length')
         edges.append((nodes.index(elt[0]), nodes.index(elt[1]), l[0]))
     return len(nodes), edges
+
