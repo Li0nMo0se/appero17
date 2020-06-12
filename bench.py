fc21950745e82806ac7d31d0f5f6ca11c6223df9
Ilan Guenet <ilan.guenet@epita.fr>
2020-06-12 17:31:58 +0200

    [UPDATE][BENCH] Update bench for directed graph benchs

--- bench.py
+++ bench.py
@@ -4,14 +4,30 @@ import graph
 import sys
 
 
-def solve(benchmark_name="Benchmark"):
+def solve_directed():
+    graphs = []
+    directed = True
+    for i in (1, 25, 50, 75, 100, 125, 150, 175, 200):
+        num_vertices, edges_list = graph.generate_connected_undirected_graph(
+            i, directed)
+        graphs.append((num_vertices, edges_list))
+
+    b = bench("directed")
+    for num_vertices, edges_list in graphs:
+        path = sm.solve(directed, num_vertices, edges_list)
+        b(f"{num_vertices} vertices, {len(edges_list)} edges")
+    b.stop()
+    b.display()
+
+
+def solve_undirected():
     graphs = []
     directed = False
     for i in (1, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250):
         num_vertices, edges_list = graph.generate_connected_undirected_graph(i)
         graphs.append((num_vertices, edges_list))
 
-    b = bench(benchmark_name)
+    b = bench("undirected")
     for num_vertices, edges_list in graphs:
         sm.solve(directed, num_vertices, edges_list)
         b(f"{num_vertices} vertices, {len(edges_list)} edges")
@@ -21,8 +37,8 @@ def solve(benchmark_name="Benchmark"):
 
 if __name__ == '__main__':
     if len(sys.argv) > 2:
-        print(f"Usage: python3 {sys.argv[0]} [benchmark name]", file=sys.stderr)
-    elif len(sys.argv) == 2:
-        solve(sys.argv[1])
+        print(f"Usage: python3 {sys.argv[0]} [benchmark name]",
+              file=sys.stderr)
     else:
-        solve()
+        # solve_undirected()
+        solve_directed()
